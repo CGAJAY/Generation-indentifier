5f31350774485087636b33cfc32a8f7c42f62f38
CGAJAY <jmugendi737@gmail.com>
2024-06-26 20:06:17 +0300

    rendered in the DOM

--- script.js
+++ script.js
@@ -2,6 +2,79 @@ let generationForm = document.getElementById(
 	"generation-form"
 );
 
+let generationsInfo = [
+	{
+		startYear: 2013,
+		endYear: parseInt(new Date().getFullYear()),
+		genName: "Generation Alpha",
+	},
+	{
+		startYear: 1997,
+		endYear: 2012,
+		genName: "Generation Z",
+	},
+	{
+		startYear: 1981,
+		endYear: 1996,
+		genName: "Millennial",
+	},
+	{
+		startYear: 1965,
+		endYear: 1980,
+		genName: "Generation X",
+	},
+	{
+		startYear: 1946,
+		endYear: 1964,
+		genName: "Baby Boomer",
+	},
+	{
+		startYear: 1928,
+		endYear: 1945,
+		genName: "Silent Generation",
+	},
+	{
+		startYear: 1901,
+		endYear: 1927,
+		genName: "G.I Generation",
+	},
+	{
+		startYear: 1883,
+		endYear: 1900,
+		genName: "Lost Generation",
+	},
+];
+
+function orderingGensInfo() {}
+
+function getGeneration2(year) {
+	let low = 0;
+	let high = generationsInfo.length - 1;
+
+	console.time();
+	while (low <= high) {
+		let mid = Math.floor((low + high) / 2);
+		// console.log(generationsInfo[mid]);
+		if (
+			year >= generationsInfo[mid].startYear &&
+			year <= generationsInfo[mid].endYear
+		) {
+			console.timeEnd();
+			return generationsInfo[mid];
+		} else if (year > generationsInfo[mid].startYear) {
+			high = mid - 1;
+		} else {
+			low = mid + 1;
+		}
+	}
+
+	return "Unknown Generation";
+}
+
+// Example usage
+const userYear = 1890;
+console.log(getGeneration2(userYear)); // Millennial
+
 function getGeneration() {
 	let generation = " ";
 	let birthdate = document.getElementById("date").value;
@@ -9,35 +82,24 @@ function getGeneration() {
 	const birthYear = parseInt(dateObject.getFullYear());
 	const currentYear = new Date().getFullYear();
 
-	switch (true) {
-		case birthYear >= 2013 && birthYear <= currentYear:
-			generation = "Generation Alpha";
-			break;
-		case birthYear >= 1997 && birthYear <= 2012:
-			generation = "Generation Z";
-			break;
-		case birthYear >= 1981 && birthYear <= 1996:
-			generation = "Millennial";
-			break;
-		case birthYear >= 1965 && birthYear <= 1980:
-			generation = "Generation X";
-			break;
-		case birthYear >= 1946 && birthYear <= 1964:
-			generation = "Baby Boomer";
-			break;
-		case birthYear >= 1928 && birthYear <= 1945:
-			generation = "Silent Generation";
-			break;
-		case birthYear >= 1901 && birthYear <= 1927:
-			generation = "G.I Generation";
-			break;
-		case birthYear >= 1883 && birthYear <= 1900:
-			generation = "Lost Generation";
-			break;
-		default:
-			generation = "Unknown Generation";
-			console.log(birthYear);
-			break;
+	if (birthYear >= 2013 && birthYear <= currentYear) {
+		generation = "Generation Alpha";
+	} else if (birthYear >= 1997 && birthYear <= 2012) {
+		generation = "Generation Z";
+	} else if (birthYear >= 1981 && birthYear <= 1996) {
+		generation = "Millennial";
+	} else if (birthYear >= 1965 && birthYear <= 1980) {
+		generation = "Generation X";
+	} else if (birthYear >= 1946 && birthYear <= 1964) {
+		generation = "Baby Boomer";
+	} else if (birthYear >= 1928 && birthYear <= 1945) {
+		generation = "Silent Generation";
+	} else if (birthYear >= 1901 && birthYear <= 1927) {
+		generation = "G.I Generation";
+	} else if (birthYear >= 1883 && birthYear <= 1900) {
+		generation = "Lost Generation";
+	} else {
+		generation = "Unknown Generation";
 	}
 	return generation;
 }
@@ -45,43 +107,43 @@ function getGeneration() {
 function getGenerationImage() {
 	let generation = getGeneration();
 	let generationPhoto;
-	switch (true) {
-		case generation === "Generation Alpha":
+	switch (generation) {
+		case "Generation Alpha":
 			generation = `You belong to ${generation}`;
 			generationPhoto = "images/generation-Alpha.webp";
 			break;
-		case generation === "Generation Z":
+		case "Generation Z":
 			generation = `You belong to ${generation}`;
-			generationPhoto = ".images/generation-z.avif";
+			generationPhoto = "images/generation-z.avif";
 			break;
-		case generation === "Millennial":
+		case "Millennial":
 			generation = `You belong to ${generation}`;
-			generationPhoto = ".images/millennial.jpg";
+			generationPhoto = "images/millennial.jpg";
 			break;
-		case generation === "Generation X":
+		case "Generation X":
 			generation = `You belong to ${generation}`;
-			generationPhoto = ".images/generation-x.jpg";
+			generationPhoto = "images/generation-x.jpg";
 			break;
-		case generation === "Baby Boomer":
+		case "Baby Boomer":
 			generation = `You belong to ${generation}`;
-			generationPhoto = ".images/baby-boomer.jpg";
+			generationPhoto = "images/baby-boomer.jpg";
 			break;
-		case generation === "Silent Generation":
+		case "Silent Generation":
 			generation = `You belong to ${generation}`;
-			generationPhoto = ".images/silent-generation.jpg";
+			generationPhoto = "images/silent-generation.jpg";
 			break;
-		case (generation = "G.I Generation"):
+		case "G.I Generation":
 			generation = `You belong to ${generation}`;
-			generationPhoto = ".images/g-i-generation.jpg";
+			generationPhoto = "images/g-i-generation.jpg";
 			break;
-		case (generation = "Lost Generation"):
+		case "Lost Generation":
 			generation = `You belong to ${generation}`;
-			generationPhoto = ".images/lost-generation.jpg";
+			generationPhoto = "images/lost-generation.jpg";
 			break;
 		default:
 			generation = "Unknown Generation";
 			generation = `Ooops that's an ${generation} try again `;
-			generationPhoto = ".images/unknown-generation.jpg";
+			generationPhoto = "images/unknown-generation.jpg";
 			break;
 	}
 
@@ -91,9 +153,8 @@ function getGenerationImage() {
 
 function render() {
 	let generationObj = getGenerationImage();
-	let container = document.querySelector(".container");
-	let results = document.createElement("div");
-	results.innerHTML = "";
+	let results = document.getElementById("result");
+	results.innerHTML = " ";
 
 	let generationImageCont = document.createElement("img");
 	let name = document.getElementById("name").value;
@@ -108,8 +169,6 @@ function render() {
 	results.appendChild(nameContainer);
 	generationImageCont.src = generationObj.photo;
 	results.appendChild(generationImageCont);
-	container.appendChild(results);
-	// results.innerHTML = "";
 
 	document.getElementById("name").value = "";
 	document.getElementById("name").placeholder =
